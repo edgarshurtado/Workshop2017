9586a9c1c534b9d687a05edc6a865f145dd79b22
Edgar S. Hurtado <edgar.sanchez@madisonmk.com>
2017-01-11 13:39:44 +0100

    Add little script for sftp connection

---
+++ PHPSECLIB/README.md
@@ -0,0 +1,5 @@
+Another example [Connecting to SFTP with PHP](http://andy-carter.com/blog/connecting-to-sftp-with-php)
+
+It's needed the library phpseclib for make it work:
+
+`composer require phpseclib/phpseclib`
---
+++ PHPSECLIB/index.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use phpseclib\Crypt\RSA;
+use phpseclib\Net\SFTP;
+
+$sftp = new SFTP("domain"); // Just the domain, do not put the dir
+                            // It can be a IP as well
+
+$Key = new RSA();
+
+$Key->loadKey(file_get_contents("/route/to/certificate"));
+
+if(!$sftp->login("madison", $Key)){
+    echo "error!";
+}
+
+$sftp->chdir("/some/dir/path");
+
+var_dump($sftp->nlist()); // List all the files in the dir;
